294bb47539c66e8ca52472bb6000ee961299f0b6
LordPompax <capitaine_ted@hotmail.fr>
2019-10-13 23:12:51 +0200

    1er commit

--- app.js
+++ app.js
@@ -3,27 +3,55 @@ const mysql = require('mysql')
 const app = express()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
-
+const session = require('express-session')
+const cookie = require('cookie-parser')
 const port = 8080
 const domain = 'http://localhost:' + port + '/'
+// let pseudo = null
 
 app.set('view engine', 'ejs')
+app.set('trust proxy', 1)
 
+app.use(cookie())
+app.use(session({
+	secret : 'azeazeazea',
+	resave : false,
+	saveUninitialized : true,
+	cookie : {secure : false}
+}))
 app.use('/assets/', express.static('public'))
 app.use(express.urlencoded({extended : true}))
 
 app.get('/', (req, res) => {
+	// console.log(req.session.pseudo + '3')
 	res.render('pages/index')
 })
+app.get('/main', (req, res) => {
+	// if (req.session.pseudo){
+		// console.log(req.session.pseudo + '2')
+		res.render('pages/msg')
+		let pseudo = req.session.pseudo
+	// }
+	// else{
+		// res.redirect('/')
+	// }
+})
+// app.post('/', (req, res) => {
+// 	if (req.body.pseudo !== undefined && req.body.pseudo !== '') {
+// 		req.session.pseudo = req.body.pseudo
+// 		// console.log(req.session.pseudo + '1')
+// 		res.redirect('/main')
+// 	}
+// })
 
 //io.emit('some event', {for : 'everyone'})
 
-io.on('connection', (socket) => {
-	io.emit('conn', 'une personne vien de ce connecter')
-	socket.on('msg', (pseudo, msg) => {
-		if (msg !== undefined && msg !== '') {
-			if (pseudo === undefined || pseudo === '') pseudo = "Annon"
-			io.emit('msg2', pseudo, msg)
+io.on('connection', socket => {
+	//io.emit('conn', pseudo + ' vien de ce connecter')
+	socket.on('msg', data => {
+		if (data.pseudo !== undefined && data.msg !== '') {
+			if (data.pseudo === undefined || data.pseudo === '') pseudo = "Annon"
+			socket.broadcast.emit('msg', data.pseudo, data.data)
 		}
 	})
 })
--- public/code.js
+++ public/code.js
@@ -1,14 +1,20 @@
-const socket = io()
 $(() => {
+	const socket = io() // 'http://localhost:8080/main'
+	// const pseudo = prompt('pseudo'),
+
 	$('#form').submit((e) => {
 		e.preventDefault()
-		socket.emit('msg', $('#pseudo').val(), $('#msg').val() )
-		$('#msg').val('')
+		let msg = $('#msg').val()
+		socket.emit('msg', {pseudo : pseudo, msg : msg})
+		//$('.msg_contenue').append(pseudo + ' : ' + msg + '<hr/>')
+		msg.val('')
 		return false;
 	})
-	socket.on('msg2', (pseudo, msg) => {
-		$('.msg_contenue').append(pseudo + ' : ' + msg + '<hr/>')
+
+	socket.on('msg', (data) => {
+		$('.msg_contenue').append(data.pseudo + ' : ' + data.msg + '<hr/>')
 	})
+
 	socket.on('conn', (status) => {
 		$('.msg_contenue').append(status + '<hr/>')
 	})
